d9eb9816772d582151add371f858fbd1445d7808
Jacob Malcom <jmalcom@defenders.org>
2016-08-02 06:31:03 -0400

    Update roxygen for get_hier_desc.

--- R/utils.R
+++ R/utils.R
@@ -2,10 +2,10 @@
 
 #' Get description data for a level of the NVC hierarchy
 #'
-#' For natural assemblages, one of: Class, Subclass, Formation, Division,
-#' Macrogroup, Group, Alliance, Association. For human-created features, one of:
-#' Cultural Class, Cultural Subclass, Cultural Formation, Cultural Subformation,
-#' Cultural Type, Cultural Group, Cultural Subgroup
+#' For natural assemblages, level is one of: Class, Subclass, Formation,
+#' Division, Macrogroup, Group, Alliance, Association. For human-created
+#' features, one of: Cultural Class, Cultural Subclass, Cultural Formation,
+#' Cultural Subformation, Cultural Type, Cultural Group, Cultural Subgroup
 #'
 #' @param level The hierarchical level of the NVC to extract
 #' @return A data.frame with global ID, colloquial name, and the type concept
