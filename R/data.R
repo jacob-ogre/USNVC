998f686ef945e9df426afc791c689d139a785624
Jacob Malcom <jmalcom@defenders.org>
2016-08-10 10:49:26 -0400

    Add more data documentation

--- R/data.R
+++ R/data.R
@@ -239,7 +239,7 @@
 #' @source https://www1.usgs.gov/csas/nvcs_data/USNVCFullDownload.zip
 "d_subnation"
 
-#' Relationships of USDA Forest Service ecoregions to NVC categories
+#' Relationships of USDA Forest Service ecoregion fields
 #'
 #' Unclear whether the 2007 Forest Service ecoregions are newer or older than
 #' the 'plain' Forest Service ecoregions. Also unclear where the values in
@@ -258,7 +258,7 @@
 #' @source https://www1.usgs.gov/csas/nvcs_data/USNVCFullDownload.zip
 "d_usfs_ecoregion"
 
-#' Relationships of 2007 USDA Forest Service ecoregions to NVC categories
+#' Relationships of 2007 USDA Forest Service ecoregion fields
 #'
 #' Unclear whether the 2007 Forest Service ecoregions are newer or older than
 #' the 'plain' Forest Service ecoregions. Also unclear where the values in
@@ -301,3 +301,153 @@
 #' }
 #' @source https://www1.usgs.gov/csas/nvcs_data/USNVCFullDownload.zip
 "Reference"
+
+#' NVC Unit table as a data.frame
+#'
+#' The U.S. National Vegetation Classification (NVC) is provided as a series of
+#' tables at \url{http://usnvc.org/explore-classification/}.
+#'
+#' @format A tibble with 8192 rows and 14 columns.
+#' \describe{
+#' \item{\code{ELEMENT_GLOBAL_ID}}{The unique identifier of the NVC category}
+#' \item{\code{PARENT_ID}}{The identifier of the parent of the NVC category}
+#' \item{\code{D_CLASSIFICATION_LEVEL_ID}}{A factor corresponding to the level
+#'             of the hierarchy}
+#' \item{\code{elementUID}}{ELEMENT_GLOBAL_ID prefixed with '2.', presumably to
+#'             identify it as deriving from version 2 of the standard}
+#' \item{\code{classificationCode}}{An alpha-numeric sequence indicating the
+#'             placement of the category in the NVC hierarchy}
+#' \item{\code{databaseCode}}{An identifier for creating an SQL database}
+#' \item{\code{Status}}{Whether the category has been accepted into the NVC}
+#' \item{\code{colloquialName}}{A 'common' name for the category, if available}
+#' \item{\code{scientificName}}{A name for the category, which may include
+#'             scientific names of characteristic taxa}
+#' \item{\code{formattedScientificName}}{'scientificName' with HTML formatting}
+#' \item{\code{translatedName}}{'scientificName' with common name substitution}
+#' \item{\code{hierarchyLevel}}{Name of the level of the hierarchy}
+#' \item{\code{unitSort}}{An alpha-numeric code indicating the placement of the
+#'             category in the NVC; different from 'classificationCode'}
+#' \item{\code{USstatus}}{A code ???}
+#' }
+#' @source https://www1.usgs.gov/csas/nvcs_data/USNVCFullDownload.zip
+"Unit"
+
+#' NVC UnitDescription table as a data.frame
+#'
+#' The U.S. National Vegetation Classification (NVC) is provided as a series of
+#' tables at \url{http://usnvc.org/explore-classification/}.
+#'
+#' @format A tibble with 8192 rows and 29 columns.
+#' \describe{
+#' \item{\code{ELEMENT_GLOBAL_ID}}{The unique identifier of the NVC category}
+#' \item{\code{parentKey}}{The 'databaseCode' of the parent of a category}
+#' \item{\code{parentName}}{Another representation of the parent of a category}
+#' \item{\code{typeConceptSentence}}{A concise sentence describing the category}
+#' \item{\code{typeConcept}}{Text description of the conceptual model of the
+#'             category}
+#' \item{\code{classificationComments}}{Comments about the classification}
+#' \item{\code{similarNVCtypesComments}}{Comments referencing similar categories}
+#' \item{\code{diagnosticCharacteristics}}{Text description (with HTML tags) of
+#'             the characteristics that differentiate the category}
+#' \item{\code{Rationale}}{Unknown; all NA}
+#' \item{\code{Physiognomy}}{Text description (with HTML tags) of the
+#'             physiognomic characteristics (structure) of the category}
+#' \item{\code{Floristics}}{Text description (with HTML tags) of the
+#'             floral characteristics (species/groups) of the category}
+#' \item{\code{Dynamics}}{Text description (with HTML tags) of the
+#'             physical/ecological dynamics of the category}
+#' \item{\code{Environment}}{Text description (with HTML tags) of the
+#'             environmental characteristics (e.g., climate) of the category}
+#' \item{\code{Analysis}}{Unknown; all NA}
+#' \item{\code{Range}}{Text description of the category's geographical spread
+#'             and/or relationship to other classifications}
+#' \item{\code{spatialPattern}}{Categorical designation of how the NVC category
+#'             is typically arranged, e.g., large patch vs. linear}
+#' \item{\code{Nations}}{Comma-separated string of two-letter country codes
+#'             indicating nations where the category is found}
+#' \item{\code{Subnations}}{Comma-separated string of two-letter state and
+#'             province codes indicating where the category is found}
+#' \item{\code{federalLands}}{Semicolon-separated string of agency and site
+#'             where the category is found}
+#' \item{\code{tncEcoregions}}{Comma-separated string relating the category to
+#'             one or more TNC ecoregion}
+#' \item{\code{usfsEcoregions}}{Comma-separated string relating the category to
+#'             one or more USDA Forest Service ecoregion}
+#' \item{\code{omernikEcoregions}}{Unknown; all NA}
+#' \item{\code{classif_confidence_id}}{A code describing Low:High confidence of
+#'             the category classification; see \link{d_classif_confidence}}
+#' \item{\code{confidenceComments}}{Text describing how/why the confidence level
+#'             was assigned; mostly empty}
+#' \item{\code{Synonymy}}{Text (with HTML tags) describing synonyms or approx.
+#'             synonyms of the category to other classifications}
+#' \item{\code{primaryConceptSource}}{Reference of the authority of the concept}
+#' \item{\code{descriptionAuthor}}{The person/entity who authored the
+#'             'description' (perhaps the typeConcept?)}
+#' \item{\code{Acknowledgements}}{Text giving acknowledgement}
+#' \item{\code{versionDate}}{Text date stamp, format = mm/dd/yyy}
+#' }
+#' @source https://www1.usgs.gov/csas/nvcs_data/USNVCFullDownload.zip
+"UnitDescription"
+
+#' Relationships of USDA Forest Service ecoregions to NVC categories
+#'
+#' Appears to be an expansion of the \code{usfsEcoregions} field in
+#' \link{UnitDescription} with information on occurrence added in.
+#'
+#' @format A data.frame with 45914 rows and 3 columns
+#' \describe{
+#' \item{\code{ELEMENT_GLOBAL_ID}}{The unique identifier of the NVC category}
+#' \item{\code{USFS_ECOREGION_ID}}{Numeric code for USFS ecoregion}
+#' \item{\code{D_OCCURRENCE_STATUS_ID}}{Numeric code for occurrence status}
+#' }
+#' @source https://www1.usgs.gov/csas/nvcs_data/USNVCFullDownload.zip
+"UnitXEcoregionUsfs"
+
+#' Relationships of 2007 USDA Forest Service ecoregions to NVC categories
+#'
+#' Unclear of the provenance of this data; may be a multi-way \code{JOIN}.
+#'
+#' @format A data.frame with 10484 rows and 3 columns
+#' \describe{
+#' \item{\code{ELEMENT_GLOBAL_ID}}{The unique identifier of the NVC category}
+#' \item{\code{USFS_ECOREGION_2007_ID}}{Numeric code for 2007 USFS ecoregion}
+#' \item{\code{D_OCCURRENCE_STATUS_ID}}{Numeric code for occurrence status}
+#' }
+#' @source https://www1.usgs.gov/csas/nvcs_data/USNVCFullDownload.zip
+"UnitXEcoregionUsfs2007"
+
+#' Relationships of references to NVC categories
+#'
+#' @format A data.frame with 77150 rows and 2 columns
+#' \describe{
+#' \item{\code{ELEMENT_GLOBAL_ID}}{The unique identifier of the NVC category}
+#' \item{\code{REFERENCE_ID}}{Numeric code for reference}
+#' }
+#' @source https://www1.usgs.gov/csas/nvcs_data/USNVCFullDownload.zip
+"UnitXReference"
+
+#' Relationships among NVC categories (similarities)
+#'
+#' Unclear how this table was derived.
+#'
+#' @format A data.frame with 11739 rows and 2 columns
+#' \describe{
+#' \item{\code{ELEMENT_GLOBAL_ID}}{The unique identifier of the NVC category}
+#' \item{\code{SIMGLOBAL_ID}}{The unique identifier of the similar NVC category}
+#' }
+#' @source https://www1.usgs.gov/csas/nvcs_data/USNVCFullDownload.zip
+"UnitXSimilarUnit"
+
+#' Relationships of subnation IDs to NVC categories
+#'
+#' @format A data.frame with 32934 rows and 4 columns
+#' \describe{
+#' \item{\code{ELEMENT_GLOBAL_ID}}{The unique identifier of the NVC category}
+#' \item{\code{SUBNATION_ID}}{The unique identifier of the subnation entity}
+#' \item{\code{D_CURR_PRESENCE_ABSENCE_ID}}{Code for presence/absence}
+#' \item{\code{D_DIST_CONFIDENCE_ID}}{Numeric code for distribution confidence}
+#' }
+#' @seealso \link{d_dist_confidence} \link{d_curr_presence_absence}
+#' @source https://www1.usgs.gov/csas/nvcs_data/USNVCFullDownload.zip
+"UnitXSimilarUnit"
+
